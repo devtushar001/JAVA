081837dde14b59cf4750371203d4b5c3b162f869
devtushar001 <tc341269@gmail.com>
2024-11-13 01:20:42 -0800

    comparison and bitwise operators

--- JavaCT/JavaCT03/operators.java
+++ JavaCT/JavaCT03/operators.java
@@ -4,7 +4,7 @@ public class operators {
    public static void main(String[] args) {
       // double x = 3.9847;
       // System.out.println(x);
-      int a = 4, b = 2;
+      // int a = 4, b = 2;
       // System.out.println(a + b); //6
       // System.out.println(a - b); //2
       // System.out.println(a * b); //8
@@ -15,11 +15,13 @@ public class operators {
       // System.out.println(a*=b); //8
       // System.out.println(a/=b); //4
       // System.out.println(a%=b); //0
-      System.out.println(a>b); //true
-      System.out.println(a<b); //false
-      System.out.println(a>=b); //true
-      System.out.println(a<=b); //false
-      System.out.println(a==b); //false
-      System.out.println(a!=b); //true
+      // System.out.println(a>b); //true
+      // System.out.println(a<b); //false
+      // System.out.println(a>=b); //true
+      // System.out.println(a<=b); //false
+      // System.out.println(a==b); //false
+      // System.out.println(a!=b); //true
+      System.out.println((5>2) && (2>3) && (4>3)); //false,
+      System.out.println((5>2) || (2>3) && (4>3)); //true, works left to rightpm 
    }
 }
