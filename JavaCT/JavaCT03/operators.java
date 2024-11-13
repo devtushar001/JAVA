ef894bf6509241f0041dd695088cefadc61f1e66
devtushar001 <tc341269@gmail.com>
2024-11-13 00:59:59 -0800

    comparison operator

--- JavaCT/JavaCT03/operators.java
+++ JavaCT/JavaCT03/operators.java
@@ -10,10 +10,16 @@ public class operators {
       // System.out.println(a * b); //8
       // System.out.println(a / b); //2
       // System.out.println(a % b); //0
-      System.out.println(a+=b); //6
-      System.out.println(a-=b); //4
-      System.out.println(a*=b); //8
-      System.out.println(a/=b); //4
-      System.out.println(a%=b); //0
+      // System.out.println(a+=b); //6
+      // System.out.println(a-=b); //4
+      // System.out.println(a*=b); //8
+      // System.out.println(a/=b); //4
+      // System.out.println(a%=b); //0
+      System.out.println(a>b); //true
+      System.out.println(a<b); //false
+      System.out.println(a>=b); //true
+      System.out.println(a<=b); //false
+      System.out.println(a==b); //false
+      System.out.println(a!=b); //true
    }
 }
