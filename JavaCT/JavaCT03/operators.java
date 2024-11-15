f9780136d949a4b655b1c491c4134e91e09ac86f
devtushar001 <tc341269@gmail.com>
2024-11-15 01:28:30 -0800

    Bitwise Operator

--- JavaCT/JavaCT03/operators.java
+++ JavaCT/JavaCT03/operators.java
@@ -23,21 +23,38 @@ public class operators {
       // System.out.println(a!=b); //true
       // System.out.println((5>2) && (2>3) && (4>3)); //false,
       // System.out.println((5>2) || (2>3) && (4>3)); //true, works left to right
-      int x = 4;
-      int y = x++;
-      System.out.println(x); //5
-      System.out.println(y); //4
-      int z = ++x;
-      System.out.println(x); //6
-      System.out.println(z); //6 same as (-)
+      // int x = 4;
+      // int y = x++;
+      // System.out.println(x); //5
+      // System.out.println(y); //4
+      // int z = ++x;
+      // System.out.println(x); //6
+      // System.out.println(z); //6 same as (-)
+
+      // int a = 5;
+      // int b = a--;
+      // System.out.println(a); //4
+      // System.out.println(b); //5
+
+      // int c = --a;
+      // System.out.println(a); //3
+      // System.out.println(c); //3
+
+      // int x = 7;
+      // int y = x++;
+      // int z = ++y;
+      // System.out.println(x); //8
+      // System.out.println(y); //8
+      // System.out.println(z); //8
+
+      // int x = 7;
+      // int y = x++ + ++x + x++ + x++;
+      // System.out.println(x); //11
+      // System.out.println(y); //35
 
       int a = 5;
-      int b = a--;
-      System.out.println(a); //4
-      System.out.println(b); //5
-
-      int c = --a;
-      System.out.println(a); //3
-      System.out.println(c); //3
+      int b = a++ + ++a + a++ + a++;
+      System.out.println(a); //9
+      System.out.println(b); //27
    }
 }
---
+++ JavaCT/JavaCT04/bitwiseoperator.java
@@ -0,0 +1,11 @@
+package JavaCT.JavaCT04;
+
+public class bitwiseoperator {
+  public static void main(String[] args) {
+    int a = 6, b = 3;
+    System.out.println(a&b); //2
+    System.out.println(a|b); //7
+    System.out.println(a^b); //5
+  }
+    
+}
