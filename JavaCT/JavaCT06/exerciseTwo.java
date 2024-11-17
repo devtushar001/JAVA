f77a8fafb3321591d349d00bea8a65b1dba6a767
devtushar001 <tc341269@gmail.com>
2024-11-17 01:48:12 -0800

    Spelling Mistakes

--- JavaCT/JavaCT06/exerciseTwo.java
+++ JavaCT/JavaCT06/exerciseTwo.java
@@ -1,12 +1,15 @@
 package JavaCT.JavaCT06;
 
 public class exerciseTwo {
-   public static void main(String[] args) {
-      // checking sides of triangle
+  public static void main(String[] args) {
+    // checking sides of triangle
 
-      int a = 4, b = 4, c = 4;
-      if (a == b && b == c && c == a) System.out.println("Equilateral");
-      else if (a == b || b == c || c == a) System.out.println("Isosceles");
-      else System.out.println("Scalene");
-   }
+    int a = 2, b = 3, c = 4;
+    if (a == b && b == c && c == a)
+      System.out.println("Equilateral");
+    else if (a == b || b == c || c == a)
+      System.out.println("Isocales");
+    else
+      System.out.println("Scalene");
+  }
 }
