2de3335fa9ad69e8f14d157d0c6e2ffdbdd9bbca
devtushar001 <tc341269@gmail.com>
2024-11-16 23:56:49 -0800

    faltu cheezen

--- JavaCT/JavaCT05/exersize02.java
+++ JavaCT/JavaCT05/exersize02.java
@@ -16,12 +16,35 @@ public class exersize02 {
     // System.out.println(0x10);
     // System.out.println(0x1f);
 
-    int a = 13;
-    int b = 17;
+    // int a = 13;
+    // int b = 17;
 
-    int res = (a > b) ? a : b;
-    System.out.println(res); 
+    // int res = (a > b) ? a : b;
+    // System.out.println(res);
+    
+
+    // swapping numbers
+    // int n = 3, m = 6;
+
+    // int t;
+    //  t = n;
+    //  n = m;
+    //  m = t;
+    //  System.out.println(n); //6
+    //  System.out.println(m); // 3
+
+    // using XOR operator
+
+    //  n = n ^ m;
+    //  m = n ^ m;
+    //  n = n ^ m;
+
+    //  System.out.println(n); //6
+    //  System.out.println(m); //3
+
+    int n = 12;
+    System.out.println((n&(n - 1)) == 0);
+    //  
+    System.out.println(Integer.bitCount(n) == 1);
    }
-
-   
 }
