ddee0ff99caa0d33693f25683dd02dfc059d9cb2
devtushar001 <tc341269@gmail.com>
2024-11-17 00:42:51 -0800

    conditional statement without using curlibracket

--- JavaCT/JavaCT06/conditionalStatement.java
+++ JavaCT/JavaCT06/conditionalStatement.java
@@ -2,18 +2,26 @@ package JavaCT.JavaCT06;
 
 public class conditionalStatement {
     public static void main(String[] args) {
-        //  if (5 > 3) {
-        //     System.out.println(1);
-        //  } 
-        //  if (3 > 2) {
-        //     System.out.println(2);
-        //  }
+        // if (5 > 3) {
+        // System.out.println(1);
+        // }
+        // if (3 > 2) {
+        // System.out.println(2);
+        // }
 
-        //  if (5 > 3) {
+        // if (5 > 3) {
+        // System.out.println(1);
+        // }
+        // else {
+        // System.out.println(2);
+        // }
+
+        // if (5 < 3) System.out.println(1);
+        // System.out.println(2);
+
+        // if (5 < 3)
         //     System.out.println(1);
-        //  } 
-        //  else {
+        // else
         //     System.out.println(2);
-        //  }
     }
 }
