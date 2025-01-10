3afe66a3617702235bff61b109f0a34f35a28ee8
devtushar001 <tc341269@gmail.com>
2025-01-10 01:15:39 -0800

    running all functions

--- JavaCT/JavaCT19/methodOverloading.java
+++ JavaCT/JavaCT19/methodOverloading.java
@@ -1,16 +1,27 @@
 package JavaCT.JavaCT19;
 
 public class methodOverloading {
-     public static void main(String[] tushar) {
+     public static void main(String[] args) {
       System.out.println("Main asli hun");
+      main();
+      main(7);
+      main(new int[5]);
+      main(8);
      }
 
      public static void main() {
         System.out.println("Khali vala main hun");
      }
 
-     public static int main(int[] divesh) {
+     public static void main(int[] divesh) {
         System.out.println("int vala main hunn");
-        return 1;
+     }
+
+     public static void main(int x) {
+         System.out.println(x);
+     }
+
+     public static void main(String x) {
+         System.out.println(x);
      }
 }
