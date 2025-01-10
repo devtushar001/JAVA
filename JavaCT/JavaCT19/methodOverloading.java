4ab40dcd4e2d58f4d252dcdbe3c1930a42795059
devtushar001 <tc341269@gmail.com>
2025-01-10 01:35:41 -0800

    updating memory location in main method inside a class

--- JavaCT/JavaCT19/methodOverloading.java
+++ JavaCT/JavaCT19/methodOverloading.java
@@ -1,27 +1,38 @@
 package JavaCT.JavaCT19;
 
 public class methodOverloading {
-     public static void main(String[] args) {
-      System.out.println("Main asli hun");
-      main();
-      main(7);
-      main(new int[5]);
-      main(8);
-     }
+  static int x = 1;
 
-     public static void main() {
-        System.out.println("Khali vala main hun");
-     }
+  public static void main(String[] args) {
+    System.out.println("Main asli hun");
+    System.out.println(args);
 
-     public static void main(int[] divesh) {
-        System.out.println("int vala main hunn");
-     }
+    if (args.length == 5) {
+      System.out.println("Done");
+      return;
+    }
+    System.out.println(args.length);
+    main(new String[x++]);
 
-     public static void main(int x) {
-         System.out.println(x);
-     }
+    // main();
+    // main(7);
+    // main(new int[5]);
+    // main(8);
+  }
 
-     public static void main(String x) {
-         System.out.println(x);
-     }
+  public static void main() {
+    System.out.println("Khali vala main hun");
+  }
+
+  public static void main(int[] divesh) {
+    System.out.println("int vala main hunn");
+  }
+
+  public static void main(int x) {
+    System.out.println(x);
+  }
+
+  public static void main(String x) {
+    System.out.println(x);
+  }
 }
