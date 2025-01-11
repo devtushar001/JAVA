c3f3bf9d42b7f804325c7f97625f57e62434b6d3
devtushar001 <tc341269@gmail.com>
2025-01-11 02:19:38 -0800

    Oops In Java

---
+++ JavaCT/JavaCT20/opps.java
@@ -0,0 +1,29 @@
+package JavaCT.JavaCT20;
+
+public class opps {
+  public static void main(String[] args) {
+    P p = new P(4, 5);
+    Q q = new Q(p);
+    System.out.println(p.x + " " + p.y);
+    System.out.println(q.x + " " + q.y + " " + q.z);
+  }
+}
+
+class P {
+  int x, y;
+
+  P(int x, int y) {
+    this.x = x;
+    this.y = y;
+  }
+}
+
+class Q extends P {
+  int z;
+
+  Q(P obj ) {
+    // this.z = Z;   // Giving a problem error
+    // super(obj.x, obj.y);
+    super(5, obj.y);
+  }
+}
