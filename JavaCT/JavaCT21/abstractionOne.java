329ba66d9a717023af18322f448b34bf5c5bbd9a
devtushar001 <tc341269@gmail.com>
2025-01-12 00:54:10 -0800

    Oops abstraction concepts

--- JavaCT/JavaCT21/abstractionOne.java
+++ JavaCT/JavaCT21/abstractionOne.java
@@ -2,34 +2,41 @@ package JavaCT.JavaCT21;
 
 public class abstractionOne {
   public static void main(String[] args) {
-    AB a = new AB();
+    // AB a = new AB();
     CD b = new CD();
 
-    a.show();
-    a.display();
+    // a.show();
+    // a.display();
     b.show();
-    b.display();
+    // b.display();
     b.show(5);
     b.display(7);
 
   }
 }
 
-class AB {
+abstract class AB {
   public void show() {
     System.out.println("AB Show");
   }
 
-  public void display() {
-    System.out.println("AB display");
-  }
+  // public void display() {
+  // System.out.println("AB display");
+  // }
+  // abstract public void display() {
+  // System.out.println("AB display");
+  // }
+  abstract public void display(int a);
 }
 
 class CD extends AB {
-   public void show(int x) {
-     System.out.println("CD show");
-   }
-   public void display(int x) {
-     System.out.println("CD display");
-   }
+   
+
+  public void show(int x) {
+    System.out.println("CD show");
+  }
+
+  public void display(int x) {
+    System.out.println("CD display");
+  }
 }
